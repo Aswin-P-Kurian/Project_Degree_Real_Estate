5527fc17696443e0a1aacf0c47c261d74469a122
Aswin-P-Kurian <aswinpkurian@gmail.com>
2025-09-30 16:43:02 +0530

    Add files via upload

---
+++ documentaton sample/Tourism-Management-System-main/travel/common/footer.php
@@ -0,0 +1,60 @@
+<!-- FOOTER SECTION STARTS -->
+					
+				<div class="footer col-sm-12">
+					<div class="col-sm-4">
+						
+						<div class="footerHeading">
+							Contact Us
+						</div>
+							
+						<div class="footerText">
+							TravelMate Tours and Travels, Kochi <br> Kerala, India
+						</div>
+				
+						<div class="footerText">E-mail: bookings@travelmate.com
+						</div>
+						
+					</div>
+					
+					
+					<div class="col-sm-4">
+					</div>
+				
+					
+					<div class="col-sm-4">
+					
+						<div class="footerHeading">
+							Social Links
+						</div>
+						
+						<div class="socialLinks">
+						
+							<div class="fb">
+								TM Tours and Travels
+							</div>
+					
+				
+						
+							<div class="tw">
+							TM Tours and Travels
+							</div>
+						
+							<div class="in"> 
+								TM Tours and Travels
+							</div>
+						
+						</div> <!-- social links -->
+						
+					</div>
+						
+					<div class="col-sm-12">
+					<div class="copyrightContainer">
+						<div class="copyright">
+						Copyright &copy; TravelMate Tours and Travels
+						</div>
+					</div>
+					</div>
+							
+				</div> <!-- footer -->
+				
+			<!-- FOOTER SECTION ENDS -->
